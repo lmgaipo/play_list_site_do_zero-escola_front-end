75d3466cd1ce59b4146ca1ef73895dd7a83c4468
lmgaipo <lucasmgaipo@outlook.com>
2022-04-12 09:03:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# play_list_site_do_zero-escola_front-end
+ Criando site do zero
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinity: true,
+    speed: 500,
+    slideToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplayspeed: 500
+});
